776cfab4f083c84bda0d83b87cc1ac2cb41fd203
jackcdavey <jdavey@scu.edu>
2023-05-26 11:31:00 -0700

    Working, colors incorrect

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,32 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+// For priority setting
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.cmd === 'setPriority') {
+    let color;
+    switch (request.priority) {
+      case 'low':
+        color = 'green';
+        break;
+      case 'medium':
+        color = 'yellow';
+        break;
+      case 'high':
+        color = 'red';
+        break;
+      default:
+        color = 'grey';
+    }
+
+    chrome.tabs.group({
+      groupId: request.groupId,
+      tabIds: request.tabId,
+      createProperties: { color: color, title: request.priority }
+    }, (groupId) => {
+      sendResponse({ groupId: groupId });
+    });
+  }
+
+  return true;  
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -6,6 +6,25 @@ import './Popup.css';
 const Popup = () => {
   const [max, setMax] = useState(0);
   const [min, setMin] = useState(0);
+  const [priority, setPriority] = useState('');
+
+  const handleClick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      const tabId = tabs[0].id;
+      chrome.tabs.group({ tabIds: tabId }, groupId => {
+        chrome.runtime.sendMessage({
+          cmd: 'setPriority',
+          priority: priority,
+          groupId: groupId,
+          tabId: tabId
+        }, response => {
+          // Handle response.
+        });
+      });
+    });
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +33,16 @@ const Popup = () => {
       <Timer />
       <EditCard title={'Add Maximum'} value={max} setValue={setMax} />
       <EditCard title={'Edit Minimum'} value={min} setValue={setMin} />
+
+      <div>
+        <select value={priority} onChange={e => setPriority(e.target.value)}>
+          <option value="">--Select a Priority--</option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+        <button onClick={handleClick}>Set Priority</button>
+      </div>
     </div>
   );
 };
