f67a42b4832c235e6c33e7a4989cef57f8173a6d
dev4fun <emiliodeng98@gmail.com>
2023-05-28 21:53:02 -0700

    fomat develop branch

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -18,7 +18,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         color = 'grey';
     }
 
-    chrome.storage.local.get(['groupIds', 'groups', 'tabs'], function(data) {
+    chrome.storage.local.get(['groupIds', 'groups', 'tabs'], function (data) {
       console.log('Retrieved data:', data);
 
       let groupIds = data.groupIds || {};
@@ -31,49 +31,67 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
       if (groupIds[request.priority] !== undefined) {
         console.log('Group exists for priority, adding tab to group...');
-        chrome.tabs.group({
-          groupId: groupIds[request.priority],
-          tabIds: request.tabId
-        }, function() {
-          console.log('Tab added to existing group.');
-          tabs[request.tabId] = {
-            priority: request.priority,
+        chrome.tabs.group(
+          {
             groupId: groupIds[request.priority],
-            // Add your other attributes here.
-          };
-          chrome.storage.local.set({ tabs: tabs }, function() {
-            console.log('Updated tab data:', tabs);
-            sendResponse({ groupId: groupIds[request.priority] });
-          });
-        });
-      } else {
-        console.log('No group exists for priority, creating new group...');
-        chrome.tabs.group({
-          tabIds: request.tabId
-        }, function(newGroupId) {
-          console.log('New group created.');
-          chrome.tabGroups.update(newGroupId, { color: color, title: request.priority }, function() {
-            console.log('Group updated with color and title.');
-            groupIds[request.priority] = newGroupId;
-            groups[newGroupId] = {
-              color: color,
-              title: request.priority,
-              // Add other group properties here.
-            };
+            tabIds: request.tabId,
+          },
+          function () {
+            console.log('Tab added to existing group.');
             tabs[request.tabId] = {
               priority: request.priority,
-              groupId: newGroupId,
+              groupId: groupIds[request.priority],
               // Add your other attributes here.
             };
-            chrome.storage.local.set({ groupIds: groupIds, groups: groups, tabs: tabs }, function() {
-              console.log('Updated group and tab data:', groupIds, groups, tabs);
-              sendResponse({ groupId: newGroupId });
+            chrome.storage.local.set({ tabs: tabs }, function () {
+              console.log('Updated tab data:', tabs);
+              sendResponse({ groupId: groupIds[request.priority] });
             });
-          });
-        });
+          }
+        );
+      } else {
+        console.log('No group exists for priority, creating new group...');
+        chrome.tabs.group(
+          {
+            tabIds: request.tabId,
+          },
+          function (newGroupId) {
+            console.log('New group created.');
+            chrome.tabGroups.update(
+              newGroupId,
+              { color: color, title: request.priority },
+              function () {
+                console.log('Group updated with color and title.');
+                groupIds[request.priority] = newGroupId;
+                groups[newGroupId] = {
+                  color: color,
+                  title: request.priority,
+                  // Add other group properties here.
+                };
+                tabs[request.tabId] = {
+                  priority: request.priority,
+                  groupId: newGroupId,
+                  // Add your other attributes here.
+                };
+                chrome.storage.local.set(
+                  { groupIds: groupIds, groups: groups, tabs: tabs },
+                  function () {
+                    console.log(
+                      'Updated group and tab data:',
+                      groupIds,
+                      groups,
+                      tabs
+                    );
+                    sendResponse({ groupId: newGroupId });
+                  }
+                );
+              }
+            );
+          }
+        );
       }
     });
 
-    return true;  // Keeps the message channel open while waiting for async response.
+    return true; // Keeps the message channel open while waiting for async response.
   }
 });
