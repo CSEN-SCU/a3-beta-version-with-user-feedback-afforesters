bd5397fac596fd4d752e8f79799b42bf3b86cc83
jackcdavey <jdavey@scu.edu>
2023-05-28 20:12:48 -0700

    Fixed dupes, groups all work now

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,73 +1,79 @@
-// Store group IDs for each priority.
-let groupIds = {
-  'low': null,
-  'medium': null,
-  'high': null,
-};
+console.log('Running background script...');
 
-// Install event
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Background script installed');
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('Received message:', request);
+  if (request.cmd === 'setPriority') {
+    let color;
+    switch (request.priority) {
+      case 'low':
+        color = 'green';
+        break;
+      case 'medium':
+        color = 'yellow';
+        break;
+      case 'high':
+        color = 'red';
+        break;
+      default:
+        color = 'grey';
+    }
 
-  // Message listener
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received setPriority command");
+    chrome.storage.local.get(['groupIds', 'groups', 'tabs'], function(data) {
+      console.log('Retrieved data:', data);
 
-    if (request.cmd === 'setPriority') {
-      let color;
-      switch (request.priority) {
-        case 'low':
-          color = 'green';
-          break;
-        case 'medium':
-          color = 'yellow';
-          break;
-        case 'high':
-          color = 'red';
-          break;
-        default:
-          color = 'grey';
-      }
+      let groupIds = data.groupIds || {};
+      let groups = data.groups || {};
+      let tabs = data.tabs || {};
 
-      let groupId = groupIds[request.priority];
+      console.log('Current groupIds:', groupIds);
+      console.log('Current groups:', groups);
+      console.log('Current tabs:', tabs);
 
-      if (groupId === null) {
-        console.log('create new group');
+      if (groupIds[request.priority] !== undefined) {
+        console.log('Group exists for priority, adding tab to group...');
         chrome.tabs.group({
-          createProperties: { color: color, title: request.priority },
+          groupId: groupIds[request.priority],
           tabIds: request.tabId
-        }, function(newGroupId) {
-          console.log("Created new group with ID " + newGroupId);
-          groupIds[request.priority] = newGroupId;
-          sendResponse({ groupId: newGroupId });
+        }, function() {
+          console.log('Tab added to existing group.');
+          tabs[request.tabId] = {
+            priority: request.priority,
+            groupId: groupIds[request.priority],
+            // Add your other attributes here.
+          };
+          chrome.storage.local.set({ tabs: tabs }, function() {
+            console.log('Updated tab data:', tabs);
+            sendResponse({ groupId: groupIds[request.priority] });
+          });
         });
       } else {
-        chrome.tabs.get(request.tabId, function(tab) {
-          console.log("Tab ID " + request.tabId + " current groupId: " + tab.groupId);
-          if (tab.groupId >= 0) {
-            console.log("Ungrouping tab ID " + request.tabId);
-            chrome.tabs.ungroup(tab.id, function() {
-              console.log("Grouping tab ID " + request.tabId + " into group ID " + groupId);
-              chrome.tabs.group({
-                groupId: groupId,
-                tabIds: request.tabId
-              }, function() {
-                sendResponse({ groupId: groupId });
-              });
+        console.log('No group exists for priority, creating new group...');
+        chrome.tabs.group({
+          tabIds: request.tabId
+        }, function(newGroupId) {
+          console.log('New group created.');
+          chrome.tabGroups.update(newGroupId, { color: color, title: request.priority }, function() {
+            console.log('Group updated with color and title.');
+            groupIds[request.priority] = newGroupId;
+            groups[newGroupId] = {
+              color: color,
+              title: request.priority,
+              // Add other group properties here.
+            };
+            tabs[request.tabId] = {
+              priority: request.priority,
+              groupId: newGroupId,
+              // Add your other attributes here.
+            };
+            chrome.storage.local.set({ groupIds: groupIds, groups: groups, tabs: tabs }, function() {
+              console.log('Updated group and tab data:', groupIds, groups, tabs);
+              sendResponse({ groupId: newGroupId });
             });
-          } else {
-            console.log("Grouping tab ID " + request.tabId + " into group ID " + groupId);
-            chrome.tabs.group({
-              groupId: groupId,
-              tabIds: request.tabId
-            }, function() {
-              sendResponse({ groupId: groupId });
-            });
-          }
+          });
         });
       }
+    });
 
-      return true;
-    }
-  });
+    return true;  // Keeps the message channel open while waiting for async response.
+  }
 });
