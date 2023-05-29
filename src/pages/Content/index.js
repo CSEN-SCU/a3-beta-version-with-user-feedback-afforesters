40f4cc47beefcaca73b87067df5f712129af3661
jonathanhstock <jonathanhstock@gmail.com>
2023-05-29 15:35:45 -0700

    added button to close tab when timer is done

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,53 +1,57 @@
- import { printLine } from './modules/print';
-//import { timer } from '../Background/index';
-
 console.log('Running Content Script');
 
-console.log('Content script works!'); 
+console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-// printLine("Using the 'printLine' function from the Print Module");
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.cmd=== "TIME_IS_UP") {
-    blockScreen()
-    sendResponse({ status: "done" });
+  if (request.cmd === 'TIME_IS_UP') {
+    blockScreen();
+    sendResponse({ status: 'done' });
   }
 });
 
-// when there is a minimum time limit or maximum time limit then popup a black screen
-
-// Set the timer duration in milliseconds
-// const timerDuration = 5000; // 5 seconds
-
-// // Perform the screen blocking after the timer is up
-// setTimeout(() => {
-//   blockScreen();
-// }, timerDuration);
-
-// // Function to block the screen
+// Function to block the screen
 function blockScreen() {
-//   // Create a div element to cover the screen
-    const blockerDiv = document.createElement('div');
-    blockerDiv.style.position = 'fixed';
-    blockerDiv.style.top = '0';
-    blockerDiv.style.left = '0';
-    blockerDiv.style.width = '100%';
-    blockerDiv.style.height = '100%';
-    blockerDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    blockerDiv.style.zIndex = '9999';
+  // Create a div element to cover the screen
+  const blockerDiv = document.createElement('div');
+  blockerDiv.style.position = 'fixed';
+  blockerDiv.style.top = '0';
+  blockerDiv.style.left = '0';
+  blockerDiv.style.width = '100%';
+  blockerDiv.style.height = '100%';
+  blockerDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  blockerDiv.style.zIndex = '9999';
 
-    const img = document.createElement('img');
-    img.style.position = 'fixed';
-    img.style.top = '50%';
-    img.style.left = '50%';
-    img.style.transform = 'translate(-50%, -50%)';
-    img.style.zIndex = '10000';
-    img.src =
-      'https://www.shutterstock.com/image-vector/times-red-sign-vector-illustration-260nw-1338912215.jpg';
+  const img = document.createElement('img');
+  img.style.position = 'fixed';
+  img.style.top = '50%';
+  img.style.left = '50%';
+  img.style.transform = 'translate(-50%, -50%)';
+  img.style.zIndex = '10000';
+  img.src =
+    'https://www.shutterstock.com/image-vector/times-red-sign-vector-illustration-260nw-1338912215.jpg';
+  // Add the div element to the page
 
-    // Add the div element to the page
-    document.body.appendChild(blockerDiv);
-    document.body.appendChild(img);
+  const unblockDiv = document.createElement('button');
+  unblockDiv.classList.add('button-container-div');
+  unblockDiv.textContent = 'Close Tab';
+  unblockDiv.style.position = 'absolute';
+  unblockDiv.style.textAlign = 'center';
+  unblockDiv.style.border = '4px solid coral';
+  unblockDiv.style.height = '10vh';
+  unblockDiv.style.width = '10vh';
+  unblockDiv.style.zIndex = '10001';
+  unblockDiv.style.top = '20%';
+  unblockDiv.style.left = '47%';
+  unblockDiv.style.margin = '0';
+  unblockDiv.style.backgroundColor = 'white';
+  // Add event listener to the button
+  unblockDiv.addEventListener('click', () => {
+    // Close the current tab
+    window.close();
+  });
+
+  document.body.appendChild(blockerDiv);
+  document.body.appendChild(img);
+  document.body.appendChild(unblockDiv);
 }
-
