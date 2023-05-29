d6307e916d733515e62242d2fab062678131aa63
jackcdavey <jdavey@scu.edu>
2023-05-28 20:05:41 -0700

    Tab groups mostly working, need to fix duplicate groups still

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,4 +1,3 @@
-// For priority setting
 // Store group IDs for each priority.
 let groupIds = {
   'low': null,
@@ -6,56 +5,69 @@ let groupIds = {
   'high': null,
 };
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.cmd === 'setPriority') {
-    let color;
-    switch (request.priority) {
-      case 'low':
-        color = 'green';
-        break;
-      case 'medium':
-        color = 'yellow';
-        break;
-      case 'high':
-        color = 'red';
-        break;
-      default:
-        color = 'grey';
-    }
+// Install event
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Background script installed');
 
-    let groupId = groupIds[request.priority];
-      
-    if (groupId === null) {
-      console.log('create new group');
-      chrome.tabs.group({
-        createProperties: { color: color, title: request.priority },
-        tabIds: request.tabId
-      }, function(newGroupId) {
-        groupIds[request.priority] = newGroupId;
-        sendResponse({ groupId: newGroupId });
-      });
-    } else {
-      chrome.tabs.get(request.tabId, function(tab) {
-        if (tab.groupId >= 0) {
-          chrome.tabs.ungroup(tab.id, function() {
+  // Message listener
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Received setPriority command");
+
+    if (request.cmd === 'setPriority') {
+      let color;
+      switch (request.priority) {
+        case 'low':
+          color = 'green';
+          break;
+        case 'medium':
+          color = 'yellow';
+          break;
+        case 'high':
+          color = 'red';
+          break;
+        default:
+          color = 'grey';
+      }
+
+      let groupId = groupIds[request.priority];
+
+      if (groupId === null) {
+        console.log('create new group');
+        chrome.tabs.group({
+          createProperties: { color: color, title: request.priority },
+          tabIds: request.tabId
+        }, function(newGroupId) {
+          console.log("Created new group with ID " + newGroupId);
+          groupIds[request.priority] = newGroupId;
+          sendResponse({ groupId: newGroupId });
+        });
+      } else {
+        chrome.tabs.get(request.tabId, function(tab) {
+          console.log("Tab ID " + request.tabId + " current groupId: " + tab.groupId);
+          if (tab.groupId >= 0) {
+            console.log("Ungrouping tab ID " + request.tabId);
+            chrome.tabs.ungroup(tab.id, function() {
+              console.log("Grouping tab ID " + request.tabId + " into group ID " + groupId);
+              chrome.tabs.group({
+                groupId: groupId,
+                tabIds: request.tabId
+              }, function() {
+                sendResponse({ groupId: groupId });
+              });
+            });
+          } else {
+            console.log("Grouping tab ID " + request.tabId + " into group ID " + groupId);
             chrome.tabs.group({
               groupId: groupId,
               tabIds: request.tabId
             }, function() {
               sendResponse({ groupId: groupId });
             });
-          });
-        } else {
-          chrome.tabs.group({
-            groupId: groupId,
-            tabIds: request.tabId
-          }, function() {
-            sendResponse({ groupId: groupId });
-          });
-        }
-      });
-    }
+          }
+        });
+      }
 
-    return true;
-  }
+      return true;
+    }
+  });
 });
