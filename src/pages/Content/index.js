d50b972e7eb8190ab8a6edd1f9b31196a92855ac
jonathanhstock <jonathanhstock@gmail.com>
2023-05-29 10:34:13 -0700

    updated content timer

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,4 +1,5 @@
 import { printLine } from './modules/print';
+import { timer } from './background/index.js';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
@@ -12,3 +13,37 @@ printLine("Using the 'printLine' function from the Print Module");
 // });
 
 // when there is a minimum time limit or maximum time limit then popup a black screen
+
+// Set the timer duration in milliseconds
+const timerDuration = 5000; // 5 seconds
+
+// Perform the screen blocking after the timer is up
+setTimeout(() => {
+  blockScreen();
+}, timer.time);
+
+// Function to block the screen
+function blockScreen() {
+  // Create a div element to cover the screen
+  const blockerDiv = document.createElement('div');
+  blockerDiv.style.position = 'fixed';
+  blockerDiv.style.top = '0';
+  blockerDiv.style.left = '0';
+  blockerDiv.style.width = '100%';
+  blockerDiv.style.height = '100%';
+  blockerDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  blockerDiv.style.zIndex = '9999';
+
+  const img = document.createElement('img');
+  img.style.position = 'fixed';
+  img.style.top = '50%';
+  img.style.left = '50%';
+  img.style.transform = 'translate(-50%, -50%)';
+  img.style.zIndex = '10000';
+  img.src =
+    'https://www.shutterstock.com/image-vector/times-red-sign-vector-illustration-260nw-1338912215.jpg';
+
+  // Add the div element to the page
+  document.body.appendChild(blockerDiv);
+  document.body.appendChild(img);
+}
