6eb66f13358deb50fb77f3669dee8396b7d4bfe9
dev4fun <emiliodeng98@gmail.com>
2023-05-30 14:05:45 -0700

    update style

--- src/pages/Popup/Timer.jsx
+++ src/pages/Popup/Timer.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
+import './Timer.css';
 
 const styles = {
   red: {
     color: `#E84B4B`,
   },
   yellow: {
-    color: `rgba(238, 229, 15, 0.77)`,
+    color: `rgba(238, 229, 15, 1)`,
   },
   body: {
-    height: '66px',
+    height: '70px',
   },
 };
 
-const Timer = ({ time, timeRange, timeIsUp }) => {
+const Timer = ({ time, timeRange, timeIsUp, type = 'max' }) => {
   return (
     <div style={styles.body}>
-      {timeRange === 0 ? (
-        <p>No limits Sets</p>
-      ) : (
-        <h1 style={timeIsUp ? styles.red : styles.yellow}>
-          {time}m/{timeRange}m
-        </h1>
-      )}
-
-      {timeIsUp && <p>Maximum daily time limit reached</p>}
+      <div className="timer-counter">
+        {timeRange === 0 ? (
+          <h1 className="timer-counter-no-limit">No limits Sets</h1>
+        ) : (
+          <h1
+            className="timer-counter-number"
+            style={timeIsUp ? styles.red : styles.yellow}
+          >
+            {time}m/{timeRange}m
+          </h1>
+        )}
+      </div>
+      <div className="timer-status">
+        {timeRange && !timeIsUp && <p>Running...</p>}
+        {timeIsUp && type === 'max' && <p>Maximum daily time limit reached</p>}
+        {timeIsUp && type === 'min' && <p>Minimum daily time reached</p>}
+      </div>
     </div>
   );
 };
